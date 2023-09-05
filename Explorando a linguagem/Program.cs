145484611500b44f8ec97a7f103045b5e70511cf
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-09-05 18:59:53 -0300

    exemplos

--- Explorando a linguagem/Program.cs
+++ Explorando a linguagem/Program.cs
@@ -1,12 +1,30 @@
 ﻿
 using ExemploExplorando.Models;
+using System.Globalization;
 
-Pessoa p = new Pessoa(nome: "Samuel", sobrenome: "Lima");
+string dataString = "2022-04-27 18:00";
 
-Pessoa p2 = new Pessoa(nome: "Ana", sobrenome: "clara");
+bool sucesso = DateTime.TryParseExact(dataString, // Propria data que deseja trabalhar
+                        "yyyy-MM-dd HH:mm", // informa o formato que a data estar. (Tem que ser da mesma forma que está no DataString)
+                        CultureInfo.InvariantCulture, // Colocando um padrao para definir o que voce espera.
+                        DateTimeStyles.None, out DateTime data); // Se conseguir converter ou não coloca na variavel data
 
-Curso curso = new Curso("Dev");
 
-curso.AdicionarAluno(p);
-curso.AdicionarAluno(p2);
-curso.ListarAlunos();
+if (sucesso){
+    Console.WriteLine($"Conversão com sucesso! Data : {data}");
+}
+else{
+    Console.WriteLine($"{dataString} não e uma data valida.");
+}
+
+
+
+// Pessoa p = new Pessoa(nome: "Samuel", sobrenome: "Lima");
+
+// Pessoa p2 = new Pessoa(nome: "Ana", sobrenome: "clara");
+
+// Curso curso = new Curso("Dev");
+
+// curso.AdicionarAluno(p);
+// curso.AdicionarAluno(p2);
+// curso.ListarAlunos();
