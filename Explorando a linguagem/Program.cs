eb9c1077817915d67c213d39ab7993819acaeb73
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-09-06 15:15:34 -0300

    exception

--- Explorando a linguagem/Program.cs
+++ Explorando a linguagem/Program.cs
@@ -1,23 +1,9 @@
 ﻿
 using ExemploExplorando.Models;
-using System.Globalization;
-
-string dataString = "2022-04-27 18:00";
-
-bool sucesso = DateTime.TryParseExact(dataString, // Propria data que deseja trabalhar
-                        "yyyy-MM-dd HH:mm", // informa o formato que a data estar. (Tem que ser da mesma forma que está no DataString)
-                        CultureInfo.InvariantCulture, // Colocando um padrao para definir o que voce espera.
-                        DateTimeStyles.None, out DateTime data); // Se conseguir converter ou não coloca na variavel data
 
 
-if (sucesso){
-    Console.WriteLine($"Conversão com sucesso! Data : {data}");
-}
-else{
-    Console.WriteLine($"{dataString} não e uma data valida.");
-}
-
-
+ExemploExcecao e = new ExemploExcecao();
+e.Metodo1();
 
 // Pessoa p = new Pessoa(nome: "Samuel", sobrenome: "Lima");
 
