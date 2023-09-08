1d36aa0bc09fdceca7c67aa03bfc09dc3330e2ae
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-09-08 11:56:58 -0300

    tupla

--- Explorando a linguagem/Program.cs
+++ Explorando a linguagem/Program.cs
@@ -1,22 +1,10 @@
-﻿
-using ExemploExplorando.Models;
+﻿using ExemploExplorando.Models;
 
-Stack<int> pilha = new Stack<int>();
 
-pilha.Push(10);
-pilha.Push(11);
-pilha.Push(12);
+//Maneira mais recomendada
+(int Id, string Nome, string Sobrenome) tupla = (1, "samuel", "felipe");
 
-Console.WriteLine($"Removendo: {pilha.Pop()}");
-
-foreach(int item in pilha){System.Console.WriteLine(item);}
-
-// Pessoa p = new Pessoa(nome: "Samuel", sobrenome: "Lima");
-
-// Pessoa p2 = new Pessoa(nome: "Ana", sobrenome: "clara");
-
-// Curso curso = new Curso("Dev");
-
-// curso.AdicionarAluno(p);
-// curso.AdicionarAluno(p2);
-// curso.ListarAlunos();
+Console.WriteLine(tupla); // Todos valores
+Console.WriteLine(tupla.Id); // peguei o primeiro valor da tupla
+Console.WriteLine(tupla.Nome); 
+Console.WriteLine(tupla.Sobrenome); 
