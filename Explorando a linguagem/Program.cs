5eec353c83e3dcacf65c9faffb260c5266a24b38
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-09-16 21:21:23 -0300

    Calculadora

--- Explorando a linguagem/Program.cs
+++ Explorando a linguagem/Program.cs
@@ -1,25 +1,8 @@
-﻿using ExemploExplorando.Models;
+﻿
 
-Pessoa p1 = new Pessoa("Samuel", "Felipe");
+using ExemploExplorando.Models;
+using ExplorandoPOO.Interface;
 
-(string nome, string sobrenome) = p1;
+ICalculadora calc = new Calculadora();
 
-Console.WriteLine(nome + sobrenome);
-
-// LeituraArquivo arquivo = new LeituraArquivo();
-
-// // (bool sucesso, string[] linhasArquivos, int QuantidadeLinhas) = arquivo.LerArquivo("Arquivos/arquivoLeitura.txt");
-// var (sucesso, linhasArquivos, _ ) = arquivo.LerArquivo("Arquivos/arquivoLeitura.txt");
-
-// if (sucesso)
-// {
-//     //Console.WriteLine($"Quantidade Linhas Arquivos: {quantidadeLinhas}");
-//     foreach (var linha in linhasArquivos)
-//     {
-//         Console.WriteLine(linha);
-//     }
-// }
-// else
-// {
-//     Console.WriteLine("Não foi possível ler o arquivo");
-// }
+Console.WriteLine(calc.Dividir(10, 20));
